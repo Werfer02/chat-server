37014fe52e3d33bd86f117f999b5a469a9adba43
jacek <jacekjanusiewicz02@gmail.com>
2025-12-27 12:58:52 +0000

    second commit

--- client.cpp
+++ client.cpp
@@ -1,27 +1,44 @@
 #include <iostream>
+#include <thread>
 #include "boost/asio.hpp"
 
 using boost::asio::ip::tcp;
 
+void receiveLoop(tcp::socket& s){
+    while(true){
+        char data[1024];
+        size_t length = s.read_some(boost::asio::buffer(data));
+        std::cout << "\033[G" << "[Server]: " << std::string(data, length) << "\n";
+        std::cout << "------------------------------------------------\n[Client]: ";
+    }
+}
+
 int main(){
     std::cout << "hello world\n\n";
+    std::string ip, port;
+    std::cout << "ip to connect to:";
+    std::cin >> ip;
+    std::cout << "port:";
+    std::cin >> port;
 
     boost::asio::io_context io_context;
 
     tcp::resolver resolver(io_context);
-    auto endpoints = resolver.resolve("127.0.0.1", "4242");
+    auto endpoints = resolver.resolve(ip, port);
 
     tcp::socket socket(io_context);
+    std::cout << "connecting to server...\n";
     boost::asio::connect(socket, endpoints);
+    std::cout << "connected!\n";
 
-    std::cout << "connected to server!\n";
+    std::thread receivethread(receiveLoop, std::ref(socket));
+    receivethread.detach();
 
-    boost::asio::write(socket, boost::asio::buffer("hello from client!"));
-
-    char data[1024];
-    size_t length = socket.read_some(boost::asio::buffer(data));
-    std::cout << "received from server: " << std::string(data, length) << "\n";
-
-    boost::asio::write(socket, boost::asio::buffer("hello from server!"));
-    
+    while(true){
+        std::cout << "------------------------------------------------\n[Client]: ";
+        std::string msg;
+        std::cin >> msg;
+        std::cout << "\n";
+        boost::asio::write(socket, boost::asio::buffer(msg));
+    }
 }
--- server.cpp
+++ server.cpp
@@ -1,25 +1,44 @@
 #include <iostream>
+#include <thread>
 #include "boost/asio.hpp"
 
 using boost::asio::ip::tcp;
 
+void receiveLoop(tcp::socket& s){
+    while(true){
+        char data[1024];
+        size_t length = s.read_some(boost::asio::buffer(data));
+        std::cout << "\033[G" <<  "[Client]: " << std::string(data, length) << "\n";
+        std::cout << "[Server]: ";
+    }
+}
+
 int main(){
     std::cout << "hello world\n\n";
 
+    std::string port;
+    std::cout << "port to listen on:";
+    std::cin >> port;
+
     boost::asio::io_context io_context;
 
-    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 4242));
+    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), stoi(port)));
 
     tcp::socket socket(io_context);
 
-    std::cout << "listening on port 4242...\n";
+    std::cout << "listening on port " << port << "...\n";
     acceptor.accept(socket);
     std::cout << "client connected!\n";
 
-    char data[1024];
-    size_t length = socket.read_some(boost::asio::buffer(data));
-    std::cout << "received from client: " << std::string(data, length) << "\n";
+    std::thread receivethread(receiveLoop, std::ref(socket));
+    receivethread.detach();
 
-    boost::asio::write(socket, boost::asio::buffer("hello from server!"));
+    while(true){
+        std::cout << "[Server]: ";
+        std::string msg;
+        std::cin >> msg;
+        std::cout << "\n";
+        boost::asio::write(socket, boost::asio::buffer(msg));
+    }
     
 }
